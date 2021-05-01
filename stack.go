0434b03b02cecda171d05882cd2c2314d13c183d
TheJis <62614133+ing-thejis@users.noreply.github.com>
2021-05-01 11:01:55 -0500

    Update stack.go

--- stack.go
+++ stack.go
@@ -1,10 +1,11 @@
 package stack
 
+//structure Node
 type Node struct {
 	Data interface{}
 	Next *Node
 }
-
+//Function to create new node 
 func NewNode(v interface{}) *Node {
 	node := new(Node)
 	node.Data = v
@@ -12,22 +13,25 @@ func NewNode(v interface{}) *Node {
 	return node
 }
 
+//structure Stack 
 type Stack struct {
 	stack *Node
 }
 
+//Function to create a new stack
 func NewStack() *Stack {
 	stack := new(Stack)
 	stack.stack = nil
 	return stack
 }
 
+//Method to push node in the stack
 func (s *Stack) Push(v interface{}) {
 	node := NewNode(v)
 	node.Next = s.stack
 	s.stack = node
 }
-
+//Method to pop node in the stack
 func (s *Stack) Pop() interface{} {
 	if s.stack != nil {
 		aux := s.stack.Data
@@ -36,7 +40,7 @@ func (s *Stack) Pop() interface{} {
 	}
 	return s.stack.Data
 }
-
+//Method to show node top in the stack
 func (s *Stack) Peek() interface{} {
 	if s.stack == nil {
 		return nil
